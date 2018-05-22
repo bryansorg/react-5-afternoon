980de9ebe0fbd791a49c9e560d03e24d1d02decd
manxyrisen <bmanxy@gmail.com>
2018-05-21 19:46:37 -0500

    react5

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from "./store";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
-
